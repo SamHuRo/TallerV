5ac2aa0be4e50efdbf720fec6c6e260944b48536
SHuertasRo <samhuro3@gmail.com>
2023-03-07 09:44:05 -0500

    Creación del proyecto Estructura
    Se creo el proyecto de estructura el cual es un tipo de dato en C

---
+++ 01_ClaseEstructura/Inc/EjemploEstudiante.h
@@ -0,0 +1,14 @@
+/*
+ * EjemploEstudiante.h
+ *
+ *  Created on: 7/03/2023
+ *      Author: Samuel Huertas Rojas
+ */
+
+#ifndef EJEMPLOESTUDIANTE_H_
+#define EJEMPLOESTUDIANTE_H_
+
+
+
+
+#endif /* EJEMPLOESTUDIANTE_H_ */
